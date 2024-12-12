5bf4935451c18aa4bab96f423420149231350462
AlexZ955 <alex.zhao1024@gmail.com>
2024-12-12 16:26:31 +0800

    feat: apply a rule of prettier to formate tsx files, integrated with sanity

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,16 +6,16 @@ import Link from 'next/link'
 const Navbar = async () => {
     const session = await auth()
     return (
-        <div className='px-5 py-3 bg-white shadow-sm font-work-sans'>
-            <nav className='flex justify-between items-center'>
-                <Link href='/'>
-                    <Image src='/logo.png' alt='logo' width={144} height={30} />
+        <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/logo.png" alt="logo" width={144} height={30} />
                 </Link>
-                <div className='flex items-center gap-5'>
+                <div className="flex items-center gap-5">
                     {session && session?.user ? (
                         <>
-                            <Link href='/startup/create'>
-                                <span className='max-sm:hidden'>Create</span>
+                            <Link href="/startup/create">
+                                <span className="max-sm:hidden">Create</span>
                                 {/* <BadgePlus className="size-6 sm:hidden" /> */}
                             </Link>
 
@@ -26,11 +26,11 @@ const Navbar = async () => {
                                     await signOut({ redirectTo: '/' })
                                 }}
                             >
-                                <button type='submit'>
-                                    <span className='max-sm:hidden'>
+                                <button type="submit">
+                                    <span className="max-sm:hidden">
                                         Logout
                                     </span>
-                                    <LogOut className='size-6 sm:hidden text-red-500' />
+                                    <LogOut className="size-6 sm:hidden text-red-500" />
                                 </button>
                             </form>
 
@@ -52,7 +52,7 @@ const Navbar = async () => {
                                 await signIn('github')
                             }}
                         >
-                            <button type='submit'>Login</button>
+                            <button type="submit">Login</button>
                         </form>
                     )}
                 </div>
